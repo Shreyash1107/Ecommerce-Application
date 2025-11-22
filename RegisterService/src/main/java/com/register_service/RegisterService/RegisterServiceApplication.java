a309ef3b9c3dba84aeeee7a0cdf5020bbcb8afaf
Shreyash Kawane <shreyunkawane@gmail.com>
2025-11-22 16:02:57 +0530

    Done with Database and JPA Configuration for Register Microservice

--- RegisterService/src/main/java/com/register_service/RegisterService/RegisterServiceApplication.java
+++ RegisterService/src/main/java/com/register_service/RegisterService/RegisterServiceApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RegisterServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterServiceApplication.class, args);
+		System.out.println("This is the Register Microservice");
 	}
-
 }
